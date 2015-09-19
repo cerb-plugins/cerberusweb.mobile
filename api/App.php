43496dbcec54d70a6aa478f8c189cd5b8312dee3
Jeff Standen <jeff@webgroupmedia.com>
2015-09-19 03:02:49 -0700

    7.1 compatibility

--- api/App.php
+++ api/App.php
@@ -951,18 +951,14 @@ class Controller_Mobile extends DevblocksControllerExtension {
 		}
 	
 		if(!empty($view)) {
-			$labels = array();
-			$values = array();
+			if($active_worker) {
+				$labels = array();
+				$values = array();
+				$active_worker->getPlaceholderLabelsValues($labels, $values);
 				
-			$labels['current_worker_id'] = array(
-				'label' => 'Current Worker',
-				'context' => CerberusContexts::CONTEXT_WORKER,
-			);
-				
-			$values['current_worker_id'] = $active_worker->id;
-	
-			$view->setPlaceholderLabels($labels);
-			$view->setPlaceholderValues($values);
+				$view->setPlaceholderLabels($labels);
+				$view->setPlaceholderValues($values);
+			}
 		}
 		
 		$tpl->assign('view', $view);
